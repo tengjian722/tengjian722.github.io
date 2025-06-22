00b7f7114e9ff6027fbfab8c7a72e5a6647bea38
tengjian722 <115606034+tengjian722@users.noreply.github.com>
2025-06-22 17:49:52 +0000

    Deploying to gh-pages from @ tengjian722/tengjian722.github.io@27c13661fa4b432e7735387f337c00cfa377578b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
